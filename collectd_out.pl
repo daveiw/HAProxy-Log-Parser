53e76fdf19049f47ddf61e5d2c125bd23f180b1f
Dave Whitehouse <dw@talis.com>
2011-04-12 11:59:44 +0100

    Fix to stage1 to remove whitespace in val Added colletd output reducer Added some test data

---
+++ collectd_out.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/perl
+
+while(<>) {
+ my $logline = $_;
+ my @tokens = split( /\s+/, $_);
+ my $store = $tokens[0];
+ my @vals = split(/,/, $tokens[1]);
+
+ my $bytes = $vals[3];
+ my $frontend_id = $vals[9];
+ my $backend_id = $vals[10];
+ my $server_id = $vals[11];
+ my $tt = $vals[2];
+ my $response_code = $vals[1];
+ my $term = $vals[13];
+
+ # remove any hyphens, they are significant to collectd
+ $frontend_id =~ s/\-/_/g;
+ $backend_id =~ s/\-/_/g;
+ $server_id =~ s/\-/_/g;
+
+ $response_codes{$response_code}++;
+ $frontend{$frontend_id}++;
+ $frontend_bytes{$frontend_id} += $bytes;
+ $backend{$backend_id}++;
+ $backend_bytes{$backend_id} += $bytes;
+ $server{$server_id}++;
+ $server_bytes{$server_id} += $bytes;
+
+ if ( "$term" eq "server_error" ) {
+    $session_err{$server_id}++;
+    $session_err{$frontend_id}++;
+    $session_err{$backend_id}++;
+ }
+
+ #print "Store:$store\tfrontend:$frontend_id Backend:$backend_id Server:$server_id Time:$tt Bytes:$bytes Code:$response_code Term:$term\n";
+}
+
+
+# Collectd PUTVAL output 
+#
+foreach ( keys %frontend ) {
+        if ( length($session_err{$_}) != 0 ) {
+                $err_count = $session_err{$_};
+        } else {
+                $err_count = 0;
+        }
+        print "PUTVAL ha-00.uks.talis/haproxy/haproxy_frontend-$_ interval=60 N:$frontend{$_}:$frontend_bytes{$_}:$err_count\n";
+}
+foreach ( keys %backend ) {
+        if ( length($session_err{$_}) != 0 ) {
+                $err_count = $session_err{$_};
+        } else {
+                $err_count = 0;
+        }
+        print "PUTVAL ha-00.uks.talis/haproxy/haproxy_backend-$_ interval=60 N:$backend{$_}:$backend_bytes{$_}:$err_count\n";
+}
+foreach my $server ( keys %server ) {
+        if ( length($session_err{$server}) != 0 ) {
+                $err_count = $session_err{$server};
+        } else {
+                $err_count = 0;
+        }
+        print "PUTVAL ha-00.uks.talis/haproxy/haproxy_server-$server interval=60 N:$server{$server}:$server_bytes{$server}:$err_count\n";
+}
+foreach ( keys %response_codes ) {
+        print "PUTVAL ha-00.uks.talis/haproxy/haproxy_response_codes-$_ interval=60 N:$response_codes{$_}\n";
+}
+
--- stage1.pl
+++ stage1.pl
@@ -9,9 +9,13 @@ while(<>) {
  chomp($logline);
  my $type = "";
  my @tokens = split( /\s+/, $_);
+ #Ignore tokens 0-4 as these are syslog-ng information
+
+ # 5 is client_ip:source_port 
  my @client_data = split( /:/, $tokens[5]);
  my $client_ip = $client_data[0];
- # get date and convert to EPOCH
+
+ # 6 date and convert to EPOCH
  if ( $tokens[6] =~ m/\[([0-9]{2})\/([a-zA-z]+)\/([0-9]{4}):([0-9]{2}):([0-9]{2}):([0-9]{2})\.([0-9]+)/ ) {
 	my $day = $1;
 	my $month = $2;
@@ -21,31 +25,62 @@ while(<>) {
 	my $sec = $6;
 	$msec = $7;
 	$epoch_time = timelocal($sec, $min, $hour, $day, $month, $year);
-	$human_time = "$year-$month-$day $hour:$min:$sec.$msec";
+	$human_date = "$year-$month-$day";
+	$human_time = "$hour:$min:$sec.$msec";
  }
+
+ # 7 haproxy frontend
  my $frontend = $tokens[7];
+
+ # 8 haproxy backend and server separated by /
  my @backend_server = split( /\//, $tokens[8]);
  my $backend = $backend_server[0]; 
  my $server = $backend_server[1]; 
+
+ # 9 haproxy timing data separated by /
  my @haproxy_timings = split( /\//, $tokens[9]);
  my $tq = $haproxy_timings[0];
  my $tw = $haproxy_timings[1];
  my $tc = $haproxy_timings[2];
  my $tr = $haproxy_timings[3];
  my $tt = $haproxy_timings[4];
+
+ # 10 HTTP reponse code
  my $reponse_code = $tokens[10];
+
+ # 11 Number of bytes returned
  if ( length($tokens[11]) != 0 ) {
-     my $bytes = $tokens[11];
+     $bytes = $tokens[11];
  } else {
-     my $bytes = 0;
+     $bytes = 0;
  }
+
+ # 12 captured_request_cookie
+ # 13 captured_response_cookie
+ # 14 termination_state
+ $term_state = $tokens[14];
+ if ( $term_state =~ m/^[Ss].{3}/g ) {
+	$term_state = "server_error";
+ } else {
+	$term_state = "ok";
+ }
+
+ # 15 actconn '/' feconn '/' beconn '/' srv_conn '/' retries*
+ # 16 srv_queue '/' backend_queue
+
+ # 17 uniq ID
  my $uniq_id = $tokens[17];
+
+ # 18 HTTP method
  my $method = $tokens[18];
+ $method =~ s/\"//;
+
+ # 19 uri
  my $uri = $tokens[19];
+
  my @uri_parts = split( /\//, $uri);
  my $store = $uri_parts[2];
 
- $method =~ s/\"//;
 
  if ( $uri_parts[3] eq "services" ) {
     if ( $uri_parts[4] =~ m/facet|sparql|augment|oai-pmh|multisparql|jobs|config/ )
@@ -80,7 +115,7 @@ while(<>) {
  
  if ( $type ne "" )
  {
-   print "$store\t$type,$reponse_code,$tt,$bytes,$epoch_time\.$msec,$human_time,$method,$uniq_id,$client_ip,$frontend,$backend,$server,$uri\n";
+   print "$store\t$type,$reponse_code,$tt,$bytes,$epoch_time\.$msec,$human_date,$method,$uniq_id,$client_ip,$frontend,$backend,$server,$uri,$term_state\n";
  } else {
    #print "NOT MATCHED: $logline\n";
  }
